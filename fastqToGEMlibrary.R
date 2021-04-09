74f0cfcee7261d9863920979003e4aefceed547f
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-04-09 15:37:35 +0100

    Create fastqToGEMlibrary.R

---
+++ fastqToGEMlibrary.R
@@ -0,0 +1,49 @@
+#
+# create GEMcode library from FASTQ file (R1) for 1 sample
+#
+# Input: FASTQ file with R1 reads
+#
+#
+
+args = commandArgs(trailingOnly=TRUE)
+
+library(ShortRead)
+
+setwd('/fastq')
+
+fastqfile <- args[1]
+outFile   <- args[2]   
+
+x <- readFastq(fastqfile, full=TRUE, withIds=TRUE)  
+class(x)
+head(sread(x ))
+N <- length( sread(x ))
+head(id(x ))
+N
+
+
+A <- substr(x= as.character(sread(x) )  , start=1, stop=16   ) 
+#print(nchar(A) )
+head(A)
+B <- lapply( as.character(id(x)) , strsplit, split=" ", fixed=TRUE )
+head(B)
+
+B <- unlist(B)
+SEL <- seq(from=1, to=length(B)-1, by=2)
+B <- B[SEL]
+length(B)
+#B <- strsplit(x= as.character(id(x) )  , split=" ", fixed=TRUE )[[1]][1] 
+#B
+
+
+# The output returns 2 columns: GEMcode and ReadID
+write.table(x=cbind(A,B), file = outFile, append = TRUE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+
+#check at the end that IDs are unique
+print( length(unique(  A  )) )
+print( length(unique(  B  )) )
+
+print( N )
+
+rm(A)
+rm(B)
