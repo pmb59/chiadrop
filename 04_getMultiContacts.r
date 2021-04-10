9476ed39d85375b50a939769495299c24935ceea
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-04-10 09:54:53 +0100

    Update 04_getMultiContacts.r

--- 04_getMultiContacts.r
+++ 04_getMultiContacts.r
@@ -1,3 +1,13 @@
 Rscript getMultiContacts.r 1
 
 # where 1 bp is the GAP allowed between fragments
+
+# -----------------------------------------------------
+# It will produce the following files in the output dir:
+
+#sampleX_GapWidth_1_ChIA-view.region
+#sampleX_GapWidth_1.log
+#sampleX_PUCs_GapWidth_1.csv
+#sampleX.gemLibrary_GapWidth_1.png
+#sampleX.gemLibrary_GapWidth_1_PROCESSED.png
+#sampleX.gemLibrary_GapWidth_1.txt
