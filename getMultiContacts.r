5f61cc4975e26e4b4d0a7357d16613645bc2bb2a
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-04-12 09:58:47 +0100

    Update getMultiContacts.r

--- getMultiContacts.r
+++ getMultiContacts.r
@@ -8,7 +8,7 @@ args = commandArgs(trailingOnly=TRUE)
 # Configured parameters: 
 #-------------------------------------------------------------------
 GapWidth <- as.numeric( args[1] )  #bp
-NCORES=2
+NCORES <- 2
 setwd('/chiadrop/sampleX')
 outdir <- '/chiadrop/sampleX/outs/'
 header <- '/chiadrop/sampleX/HEADER_miaSig.txt'
@@ -56,18 +56,13 @@ setMethod("$<-", "GRanges", function(x, name, value) {
   return(x)
 }) 
 
-
 #-------------------------------------------------------------------
 # raw data stats, and reading GEM barcodes of raw reads 
 #-------------------------------------------------------------------
 library(data.table)
 mydata <- fread(GEMraw, header=FALSE)
-head(mydata)
-dim(mydata)
 
 TA <- table(mydata$V1)
-head(TA)
-
 
 write.table(paste0('Number of total raw reads in FASTQ is....', nrow(mydata)) , file = paste0(outdir,SampleID,'_GapWidth_',GapWidth,".log"), append = TRUE, quote = FALSE, sep = "\t", row.names = FALSE, col.names =FALSE)
 write.table(paste0('Number of unique raw GEM barcodes is....', length(   unique(names(TA))   )) , file = paste0(outdir,SampleID,'_GapWidth_',GapWidth,".log"), append = TRUE, quote = FALSE, sep = "\t", row.names = FALSE, col.names =FALSE)
@@ -119,7 +114,7 @@ write.table("--" , file = paste0(outdir,SampleID,'_GapWidth_',GapWidth,".log"), 
 
 #retain reads with length >= ReadLengthMin
 myBED <- myBED[ which(myBED$V11 >= ReadLengthMin ) , ]
-nrow(myBED)  #all reads passed
+nrow(myBED)  
 head(myBED)
 
 #Get rid of read-length column
@@ -127,7 +122,7 @@ myBED <- myBED[, c(1:5) ]
 head(myBED)
 
 #-------------------------------------------------------------------
-# Extend BED 500bp from its 3' end -> NO
+# Extend BED 500bp from its 3' end -> NOT DONE
 #-------------------------------------------------------------------
 forward <- which(myBED$V6 == '+')
 reverse <- which(myBED$V6 == '-')
@@ -176,14 +171,14 @@ head(X)
 dim(X)
 
 #Get rid of 'mydata' and 'myBED'
-rm(myBED)   #mydata was necessary for the Chr
+rm(myBED) 
 rm(mydata)
 
 
-#-------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 # QC the barcodes. 
 # RULE. Any barcode not matching the white list WILL BE removed as it will introduce mistakes
-#-------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 # Download 10X barcode white list
 #https://github.com/OpenGene/fastp/issues/91
 #wget https://raw.githubusercontent.com/10XGenomics/supernova/master/tenkit/lib/python/tenkit/barcodes/4M-with-alts-february-2016.txt
@@ -203,8 +198,6 @@ if(QC_barcodes ==TRUE) {   # very slow. NOT TESTED
   library(dartR)
   hammingFun <- function(SEQ, b){  
   
-  #d <- stringdist(a=SEQ, b=b, method ="hamming" ) ;
-  #d <- as.numeric( adist(x=SEQ, y=b ) )
   d <- unlist(lapply(b, utils.hamming, str2=SEQ, r=0))
 
   minD <- min( d[which(d!=0)] ) 
@@ -213,7 +206,6 @@ if(QC_barcodes ==TRUE) {   # very slow. NOT TESTED
   if (minD == 1/16) { 
     LminD <- which(d == minD  )
     if (length(LminD) == 1 ) {  return( U[which(d == 1/16)] )  }
-    #if (length(minD) > 1  )   return( SEQ )
     else { return(  SEQ   )   }
   }
   else { return(  SEQ   )   }
@@ -244,7 +236,6 @@ rm(barcodeWhiteList, MatchedBarcodes)
 
 
 
-
 #--------------------- not necessary IF RUNNING THE CODE ABOVE (it should be 0)----
 ### here we remove barcodes which will have an N
 haveN <- unlist(lapply('N' , grepl, X$GEM ))  # grepl returns logical value
@@ -256,7 +247,6 @@ if( length(which(haveN == TRUE) ) > 0 ){
   head(X)
   dim(X)
 }
-
 rm(haveN)
 #-----------------------------------------------------------------------------------
 
@@ -264,7 +254,7 @@ rm(haveN)
 # Assign GEMcode
 X$GEMcode <- unlist(lapply(X$GEM, GEMnumber))
 
-#Get rid of unnecesary columns
+# Get rid of unnecesary columns
 X <- X[, c(2:5,7)]
 
 #plot stats for the QC'ed bam file with mapped reads
@@ -297,7 +287,6 @@ write.table('Merging Reads if same GEMCode and closer than GapWidth to Create PU
 #-----------------------------------------------------------------------------------
 # Merge Read if same GEMCode and closer to GapWidth bp to Create CHROMATIN FRAGMENTS
 #-----------------------------------------------------------------------------------
-
 library(GenomicRanges)
 library(Rsamtools)
 library(bamsignals)
@@ -307,24 +296,20 @@ head(uniGEM)
 length(uniGEM)
 
 # tapply function is the most convenient here to avoid for loop
-
-Xtest <- X #[1:10000,]
+Xtest <- X 
 Xtest
-
 
 #genomic ranges
 gr <-  GRanges(
   seqnames = Rle(Xtest$chr),
   ranges = IRanges(Xtest$start, end = Xtest$end ),
-  strand = Rle(strand(c(Xtest$strand)) ) #,
-  #GEM= rep('0', length(sel))
+  strand = Rle(strand(c(Xtest$strand)) ) 
 )
 gr
-mcols(gr)$GEM <-  Xtest$GEMcode #[1:10000] #[k]
+mcols(gr)$GEM <-  Xtest$GEMcode 
 gr
 
 rm(Xtest)
-
 
 
 
@@ -332,7 +317,7 @@ MergeGenomicRanges <- function(bedData){
   
   #bedData is Genomic ranges with metadata colum GEM (barcode)
   
-  grMerged <- reduce(x=bedData, drop.empty.ranges=FALSE, min.gapwidth= GapWidth, with.revmap=FALSE,  # merge if closer than 3kb
+  grMerged <- reduce(x=bedData, drop.empty.ranges=FALSE, min.gapwidth= GapWidth, with.revmap=FALSE,  # merge if closer than GapWidth allowed
                      with.inframe.attrib=FALSE, ignore.strand=TRUE)
   
   # add GEMcode to GRanges
@@ -340,7 +325,7 @@ MergeGenomicRanges <- function(bedData){
   mcols(grMerged)$GEMcode <-  U
   
   #add read counts and keep only in ReadCounts pass a CutOff
-  mcols(grMerged)$ReadCount <- bamCount(bampath=BAM, gr=grMerged , mapqual=0, verbose=FALSE, shift = 0) #bamsignals
+  mcols(grMerged)$ReadCount <- bamCount(bampath=BAM, gr=grMerged , mapqual=0, verbose=FALSE, shift = 0) 
   grMerged <- grMerged [  which(mcols(grMerged)$ReadCount >= MinReadsInEachFragment )  ]
 
   Le <- length(grMerged)
@@ -351,43 +336,30 @@ MergeGenomicRanges <- function(bedData){
     mcols(grMerged)$GEM_purity <- 0.0
     temp_chr   <- as.character(seqnames(grMerged ))
     temp_table <- table(temp_chr )
-    #J          <- length( unique( temp_chr ) )
+
     for (h in 1: length( temp_table)){
       mcols(grMerged)$GEM_purity <-  mcols(grMerged)$GEM_purity +  ( as.numeric(temp_table[h])  / Le )^2
     }
     rm( temp_chr )
-    #rm( temp_table )   WILL USE IT BELOW
     temp_sum <- sum (temp_table)
   
 
     #categorisation based on the number of chromatin fragments
     if( Le ==1  ) { 
       mcols(grMerged)$chromatin_fragment  <-  'singleton' 
-      #mcols(grMerged)$GEM_purity <- -1    #    GEM purity for singletons = -1
-      #mcols(grMerged)$multiplexType       <-  'NA' 
-      #mcols(grMerged)$PUC       <-  U
       mcols(grMerged)$fragments  <-  1
-      #
       mcols(grMerged)$multiplexType   <- 'none'  
     }
     if( Le >=2  ) { 
       mcols(grMerged)$chromatin_fragment <- 'multiplex' # 'multiplex-intra-chromosomal' 
       mcols(grMerged)$fragments  <-  length(grMerged)
       #now each multiplex fragment can be categorised into intra-chromosomal, inter-chromosomal  , or MIXED
-      #CHRS <- unique(seqnames(grMerged)) # distinct chrs
       
       if( length(temp_table) == 1 ){  
           mcols(grMerged)$multiplexType   <- 'intra-chromosomal'
-      #  mcols(grMerged)$PUC        <-  U
-      #  mcols(grMerged)$fragments  <-  length(grMerged)
       }
       if( (length(temp_table) > 1) &  (length(temp_table) == temp_sum ) ){  
         mcols(grMerged)$multiplexType   <- 'inter-chromosomal'   
-      #  for (k in 1:length(CHRS) ){
-      #    mcols(grMerged)$PUC        <-  paste(U, CHRS[k] , sep='_' )
-      #    mcols(grMerged)$fragments  <-  length( which( as.character(seqnames(grMerged) ) == as.character(CHRS) ) )
-      #    
-      #  }
       }
       if( (length(temp_table) > 1) &  (length(temp_table) != temp_sum ) ){  
         mcols(grMerged)$multiplexType   <- 'mixed'   
@@ -398,11 +370,9 @@ MergeGenomicRanges <- function(bedData){
     rm( temp_table ) 
     rm( temp_sum  ) 
     return( as.data.table(grMerged , row.names = FALSE)[,c(1:4,6:11) ]   )  # DT object
-  
     
   } 
 }
-
 
 
 mergedX <- tapply(gr, mcols(gr)$GEM,  MergeGenomicRanges  )
@@ -415,7 +385,7 @@ length( unique(mcols(gr)$GEM) ) # number of unique GEMs
 
 
 
-mergedX2 <- rbindlist(mergedX, use.names=TRUE, fill=FALSE) #, idcol=TRUE)
+mergedX2 <- rbindlist(mergedX, use.names=TRUE, fill=FALSE) 
 
 
 unique(mergedX2$chromatin_fragment)
@@ -435,11 +405,6 @@ write.table("--" , file = paste0(outdir,SampleID,'_GapWidth_',GapWidth,".log"), 
 
 
 #Save PUTATIVE CHROMATIN COMPLEXES
-#write.csv( mergedX2, file = paste0( 'chr',Chr, '_PUCs_', nrow(mergedX2),'_GEMs_',length(mergedX), ".csv" )  , row.names = FALSE)
-#mergedX2$GEMcode <- as.integer( mergedX2$GEMcode )
-#
-#library(WriteXLS)
-#WriteXLS(x=mergedX2, ExcelFileName = paste0( 'chr',Chr, '_PUCs_', nrow(mergedX2),'_GEMs_',length(mergedX), ".xls" ), SheetNames = NULL)
 write.csv(x=mergedX2, file = paste0(outdir , SampleID, '_PUCs' ,"_GapWidth_",GapWidth, ".csv" ) , row.names = FALSE )
 
 
@@ -476,9 +441,7 @@ end_time <- format(Sys.time(), "%a %b %d %X %Y")
 write.table(paste0("Analysis started at.... ", start_time  ), file = paste0(outdir,SampleID,'_GapWidth_',GapWidth,".log"),append = TRUE, quote = FALSE, sep = "\t", row.names = FALSE, col.names =FALSE)
 write.table(paste0("Analysis has finished at ... ", end_time  ), file = paste0(outdir,SampleID,'_GapWidth_',GapWidth,".log"),append = TRUE, quote = FALSE, sep = "\t", row.names = FALSE, col.names =FALSE)
 
-
 rm(start_time, end_time)
-
 rm(mergedX3,X)
 rm(uniGEM, gr)
 
